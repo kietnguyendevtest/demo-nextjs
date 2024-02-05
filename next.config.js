87081ad36750f2b4745fa80f6e997dfa7fa5a1fa
kietnguyendevtest <kietnm@vasgroup.vn>
2024-02-05 16:21:47 +0700

    change url api

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@
 const nextConfig = {
     env: {
         //URL_API: 'http://localhost:5000/api/v1',
-        URL_API: 'https://nmkiet-api.vercel.app/api/v1',
+        URL_API: 'https://kietnm-api.vercel.app/api/v1',
     },
 }
 
